f9c8ca1f57d10dc925de65c1f034aad35ed92641
wangke <korewangggg@foxmail.com>
2024-07-05 11:33:37 +0800

    change

---
+++ wk/src/router/index.ts
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: () => import('../views/home.vue')
+        },
+        {
+            path: '/dash',
+            name: 'dash',
+            component: () => import('../views/dash.vue')
+        }
+    ]
+})
+
+export default router
---
+++ wk/src/views/dash.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="app">
+        <h1>{{ message }}</h1>
+        <button @click="changeMessage">Change Message</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'DashPage',
+    data() {
+        return {
+            message: 'Hello Vue!'
+        };
+    },
+    methods: {
+        changeMessage() {
+            this.message = 'Message changed!';
+        }
+    }
+};
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
